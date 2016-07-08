113a134c49af3e8341893a16d9fbaf8480707593
Peng.Li <peng.li@rea-group.com>
2016-07-08 10:58:23 +0800

    configs

---
+++ README.md
@@ -0,0 +1,11 @@
+```
+npm install -g eslint
+
+npm install -g eslint-plugin-extra-rules
+npm install -g eslint-plugin-xo
+npm install -g eslint-plugin-spellcheck
+npm install -g eslint-plugin-node
+
+cd project
+eslint . -c excellence-girls --no-eslintrc
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+    "env": {
+        "es6": false,
+        "node": true
+    },
+
+    "plugins": ["extra-rules", "xo"],
+
+    "extends": ["eslint:recommended", "plugin:node/recommended"],
+
+    "rules": {
+
+        // eslint built-in rules
+        "indent": ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        "semi": ["error", "always"],
+        "space-infix-ops": ["error"],
+        // "prefer-template": ["error"],
+        "max-statements": ["error", 15],
+        "complexity": ["error", 8],
+        "no-console": "warn",
+        "handle-callback-err": "error",
+        "camelcase": "error",
+        "quotes": ["warn", "single", "avoid-escape"],
+        "no-mixed-requires": ["error", { "grouping": true, "allowCall": false }],
+        "handle-callback-err": "error",
+        // "no-shadow": "error",
+        "no-mixed-spaces-and-tabs": "error",
+        "eqeqeq": "error",
+
+        // from the 3rd party `eslint-rules`
+        "extra-rules/no-long-files": ["error", 100],
+        "extra-rules/no-commented-out-code": "error",
+
+        "xo/filename-case": ["error",  {"case": "kebabCase"}],
+
+        "spellcheck/spell-checker": ["error", {
+           "comments": true,
+           "strings": true,
+           "identifiers": true,
+           "lang": "en_US",
+           "skipWords": ["req", "res", "stringify", "barcode", "fs", "params", "utf", "fd", "lodash", "api", "morgan", "uncomment", "dirname", "filename", "dev", "urlencoded", "stacktrace", "html", "mongo", "str", "mongodb", "localhost", "init", "len", "http"],
+           "skipIfMatch": ["http://[^s]*"] 
+       }]
+   }
+}
